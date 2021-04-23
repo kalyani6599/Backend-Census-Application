4ec850734050728e2ab354f6df517f1dea7cb505
Kalyani Shinde <37650147+kalyani6599@users.noreply.github.com>
2021-04-23 13:25:19 +0530

    Add files via upload

---
+++ census-app/src/main/java/com/application/census/censusapp/CensusAppApplication.java
@@ -0,0 +1,13 @@
+package com.application.census.censusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusAppApplication.class, args);
+	}
+
+}
---
+++ census-app/src/test/java/com/application/census/censusapp/CensusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.census.censusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CensusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
